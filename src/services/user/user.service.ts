9565325bc39e4f0f664e0c3d7b9195bae5ad7f75
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-04 22:09:24 +0200

    moved user service to own directory

---
+++ src/services/user/user.service.ts
@@ -0,0 +1,57 @@
+import fetch, { Response } from 'node-fetch';
+import { log } from '@root/utils';
+import uriConfig from '@root/services/serviceURI';
+const SERVICE_URI = <string>uriConfig.get('USER_SERVICE_URI');
+
+class UserService {
+	fetchUser: ({ userId }: { userId: string }) => Promise<any> = async ({ userId }) => {
+		try {
+			const data = {
+				userId,
+			};
+			const response: Response = await fetch(`${SERVICE_URI}/users/${userId}`, {
+				method: 'POST',
+				body: JSON.stringify(data),
+				headers: { 'Content-Type': 'application/json' },
+			});
+			return response;
+		} catch (error) {
+			log.error(`[user.service] - fetchUser ${error.message}`);
+			return error;
+		}
+	};
+	async fetchData(path: string, token: string) {
+		try {
+			if (!token) return new Error('Unauthorized');
+			const response: Response = await fetch(`${SERVICE_URI}/${path}`, {
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: 'Bearer ' + token,
+				},
+			});
+			return await response.json();
+		} catch (error) {
+			log.error(`[user.service] - fetchData ${error.message}`);
+			return error;
+		}
+	}
+
+	async createItem(path: string, data: any, token?: any | null) {
+		try {
+			const response = await fetch(`${SERVICE_URI}/${path}`, {
+				body: data,
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+					Authorization: 'Bearer ' + token,
+				},
+			});
+			return await response.json();
+		} catch (error) {
+			log.error(`[user.service] - createItem ${error.message}`);
+			return error;
+		}
+	}
+}
+
+export default new UserService();
