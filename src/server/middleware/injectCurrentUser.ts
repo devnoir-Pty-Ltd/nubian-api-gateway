6d293e3d006c409bffc9c6e5ee328fc510be0115
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-04 16:37:47 +0200

    injectCurrentuser session method to be used in server middleware

--- src/server/middleware/injectCurrentUser.ts
+++ src/server/middleware/injectCurrentUser.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import sessionService from 'src/services/session/session.service';
+import SessionService from '@root/services/session/session.service';
 
 const injectCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.cookies.userSessionId) {
-		const session = await sessionService.getSession({ sessionId: req.cookies.userSessionId });
-		res.locals.userSession = session;
+	if (req.cookies.sessionId) {
+		await SessionService.getSession({ sessionId: req.cookies.sessionId });
 	}
-
 	return next();
 };
 
