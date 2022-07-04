83e4783779d8cda21774c4efacb82d10795d5f83
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-04 22:34:54 +0200

    removed redundat item

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,5 @@
 import * as Query from './Query';
 import * as Mutations from './Mutations';
-import UserSession from './UserSession';
-const resolvers: any = { Mutations, Query, UserSession };
+const resolvers: any = { Mutations, Query };
 
 export default resolvers;
