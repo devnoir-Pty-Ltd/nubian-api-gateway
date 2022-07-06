644a22c55b8b5bf9da85a0c021b635c64961da0c
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-06 03:36:07 +0200

    Add message query

---
+++ src/graphql/resolvers/Query/message/getmessage.query.ts
@@ -0,0 +1,33 @@
+import createError from 'http-errors';
+import { IResolverContext } from '@root/graphql/types';
+import messageService from '@root/services/message/message.service';
+import userService from '@root/services/user/user.service';
+
+const getmessage = async (_obj: any, args: any, context: IResolverContext) => {
+	try {
+		const token: string = context.req.cookies['NUBIAN-WEBTOKEB'];
+		if (!token) throw new createError.Unauthorized();
+
+		let response = await messageService.fetchData(`messages/${args.id}`, token);
+		const users: any = await userService.fetchData('users/', token);
+
+		const currentUser = users.find((item: any) => {
+			return item.id === context.req.cookies['NUBIAN-SJID'];
+		});
+
+		response = response.filter((item: any) => {
+			if (Number(item.accountId) === currentUser.accountId) return item;
+		});
+
+		response = response.map((item: any) => {
+			item['sender'] = users.find((u: any) => u.id === item?.senderId);
+			return item;
+		});
+
+		return response;
+	} catch (error) {
+		return error;
+	}
+};
+
+export default getmessage;
