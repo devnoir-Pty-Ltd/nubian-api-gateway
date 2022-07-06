5d26b2cd1784713a3dc35a18ab81c478254bc546
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-06 03:38:30 +0200

    Add message mutation to index

--- src/graphql/resolvers/Mutations/index.ts
+++ src/graphql/resolvers/Mutations/index.ts
@@ -1,3 +1,6 @@
 export { default as signin } from './auth/signin.mutation';
 export { default as signup } from './auth/signup.mutation';
 export { default as signout } from './auth/signout.mutation';
+export { default as sendmessage } from './message/send.mutation';
+export { default as updatemessage } from './message/update.mutation';
+export { default as deleteMessage } from './message/delete.mutation';
