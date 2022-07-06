e800fb283bfde05916ef0a217f158f534524c464
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-06 03:40:44 +0200

    Extend query to include messages

--- src/graphql/resolvers/Query/index.ts
+++ src/graphql/resolvers/Query/index.ts
@@ -1,1 +1,3 @@
 export { default as signedIn } from './signedIn';
+export { default as getmessages } from './message/getmessages.query';
+export { default as getmessage } from './message/getmessage.query';
