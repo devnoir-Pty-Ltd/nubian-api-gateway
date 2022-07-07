999278690ae7ff706a5516b7ce7856081ca76dea
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-07 10:55:22 +0200

    refactor inject current user method

--- src/server/middleware/injectCurrentUser.ts
+++ src/server/middleware/injectCurrentUser.ts
@@ -1,9 +1,10 @@
+import { log } from '@root/utils';
 import { NextFunction, Request, Response } from 'express';
 import SessionService from '@root/services/session/session.service';
 
 const injectCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
-	console.log('new sesson', req.cookies);
-	if (req.cookies.nubian_sjid) {
+	if (req.cookies.nubain_sjid) {
+		log.info('[server middleware] - session injected');
 		const session = await SessionService.getSession({ sessionId: req.cookies.nubian_sjid });
 		res.locals.userSession = session;
 	}
