767a88ad3839365678549b3172be3d83a586368d
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-14 12:37:04 +0200

    Better create session mutation

--- src/graphql/resolvers/Mutations/auth/createSession.mutation.ts
+++ src/graphql/resolvers/Mutations/auth/createSession.mutation.ts
@@ -2,19 +2,12 @@ import { ISession } from './../../../../services/session/session.service';
 import authService from '@root/services/auth/auth.service';
 import { IResolverContext } from '@root/graphql/types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const createSession = async (_obj: any, args: any, context: IResolverContext) => {
-	try {
-		const session: ISession = await authService.signin({
-			...args,
-		});
-		context.res.cookie('nubain_sjid', session.id);
-		context.res.cookie('nubain_token', session.token);
-
-		return session;
-	} catch (error) {
-		return error;
-	}
+	const session: ISession = await authService.signin({
+		...args,
+	});
+	context.res.cookie('nubian_sid', session.id, { httpOnly: true });
+	return <ISession>session;
 };
 
 export default createSession;
