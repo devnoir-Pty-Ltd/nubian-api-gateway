8d49bcba55d6b96c6391ab9089bd9d0916b38414
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-14 12:24:07 +0200

    Refactor sessionInjection

--- src/server/middleware/injectCurrentUser.ts
+++ src/server/middleware/injectCurrentUser.ts
@@ -1,12 +1,13 @@
 import { log } from '@root/utils';
 import { NextFunction, Request, Response } from 'express';
 import SessionService from '@root/services/session/session.service';
+import { UserSessionType } from '@root/graphql/types';
 
 const injectCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.cookies.nubain_sjid) {
-		log.info('[server middleware] - session injected');
-		const session = await SessionService.getSession({ sessionId: req.cookies.nubian_sjid });
+	if (req.cookies.nubian_sid) {
+		const session: UserSessionType = await SessionService.getSession(req.cookies.nubian_sid);
 		res.locals.userSession = session;
+		log.info(`[server middleware] - session injected ${session.id}`);
 	}
 	return next();
 };
