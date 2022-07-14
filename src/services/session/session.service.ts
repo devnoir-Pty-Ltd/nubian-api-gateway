d9fd7fadf791e0cd7aaba764eba054ec8a1b47e8
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-14 12:18:10 +0200

    Refactor session.service

--- src/services/session/session.service.ts
+++ src/services/session/session.service.ts
@@ -1,61 +1,24 @@
 import fetch, { Response } from 'node-fetch';
 import { log } from '@root/utils';
 import uriConfig from '@root/services/serviceURI';
-import { TUser } from '@root/graphql/types';
+import { IUser } from '@root/graphql/types';
 const SERVICE_URI = <string>uriConfig.get('USER_SERVICE_URI');
 
-export type ISession = {
+export interface ISession {
 	id: string;
 	token: string;
 	userId: string;
-	user?: TUser;
-};
-class SessionService {
-	async getSession({ sessionId }: { sessionId: string }): Promise<ISession | null> {
-		try {
-			const response: Response = await fetch(`${SERVICE_URI}/auth/session/${sessionId}`);
-			const data: ISession = await response.json();
-			return data;
-		} catch (error) {
-			log.error(`[session.service] - getSession ${error.message}`);
-			if (error.status === 404) {
-				return null;
-			}
-			return error;
-		}
-	}
-	async fetchData(path: string, token: string) {
-		try {
-			log.info(`[session.service] - fetch ${token}`);
-			if (!token) return new Error('Unauthorized');
-			const response: Response = await fetch(`${SERVICE_URI}/${path}`, {
-				method: 'GET',
-				headers: {
-					'content-type': 'application/json;charset=UTF-8',
-					Authorization: 'Bearer ' + token,
-				},
-			});
-			return await response.json();
-		} catch (error) {
-			log.error(`[session.service] - fetchData ${error.message}`);
-			return error;
-		}
-	}
+	user: IUser;
+}
 
-	async createItem(path: string, data: any, token?: any | null) {
-		try {
-			const response = await fetch(`${SERVICE_URI}/${path}`, {
-				body: data,
-				headers: {
-					'content-type': 'application/json;charset=UTF-8',
-					Authorization: 'Bearer ' + token,
-				},
-			});
-			return await response.json();
-		} catch (error) {
-			log.error(`[session.service] - createItem ${error.message}`);
-			return error;
-		}
+export default class SessionService {
+	static async getSession(sessionId: string): Promise<ISession | null> {
+		const response: Response = await fetch(`${SERVICE_URI}/auth/sessions/${sessionId}`);
+		const session: ISession = await response.json().catch((err) => {
+			if (err.response.statusCode === 404) return null;
+			throw err;
+		});
+		log.info(`[sessionService - getSession]  session availale ${sessionId}`);
+		return <ISession>session;
 	}
 }
-export default new SessionService();
