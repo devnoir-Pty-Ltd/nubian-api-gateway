eb9929f5bdbdd324a5ba08d064345f5d0ced64a7
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-14 12:33:21 +0200

    Code cleanup

--- src/graphql/resolvers/Query/index.ts
+++ src/graphql/resolvers/Query/index.ts
@@ -1,3 +1,1 @@
-export { default as signedIn } from './signedIn';
-export { default as getmessages } from './message/getmessages.query';
-export { default as getmessage } from './message/getmessage.query';
+export { default as userSession } from './userSession';
