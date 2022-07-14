53dc8b00dacd3a1552a592ce9888d07158869663
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-14 12:30:51 +0200

    Fix types and schema

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,6 @@
 import * as Query from './Query';
 import * as Mutation from './Mutations';
-const resolvers: any = { Mutation, Query };
+import UserSession from './UserSession';
+const resolvers = { Mutation, Query, UserSession };
 
 export default resolvers;
