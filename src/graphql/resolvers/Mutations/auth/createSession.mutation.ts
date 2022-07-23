868caab5618f5f49ae7bc219f5e0b140224f4ae3
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-23 11:45:35 +0200

    Refactor create session

--- src/graphql/resolvers/Mutations/auth/createSession.mutation.ts
+++ src/graphql/resolvers/Mutations/auth/createSession.mutation.ts
@@ -1,6 +1,5 @@
-import { ISession } from './../../../../services/session/session.service';
 import authService from '@root/services/auth/auth.service';
-import { IResolverContext } from '@root/graphql/types';
+import { IResolverContext, UserSessionType } from '@root/graphql/types';
 
 const createSession = async (_obj: any, args: any, context: IResolverContext) => {
 	const { email, password } = args;
@@ -11,7 +10,8 @@ const createSession = async (_obj: any, args: any, context: IResolverContext) =>
 	if (session.error) return session.error;
 	context.res.cookie('nubian_sid', session.id, { httpOnly: true });
 	context.res.cookie('nubian_token', session.token, { httpOnly: true });
-	return <ISession>session;
+	context.res.cookie('nubian_aid', session.user.accountId, { httpOnly: true });
+	return <UserSessionType>session;
 };
 
 export default createSession;
