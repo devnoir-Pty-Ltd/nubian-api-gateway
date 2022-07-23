5ed8339ad5026e03ab847206c5ab8d92545d96e0
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-23 11:45:16 +0200

    Add channel mutations to list

--- src/graphql/resolvers/Mutations/index.ts
+++ src/graphql/resolvers/Mutations/index.ts
@@ -1,4 +1,10 @@
 export { default as createSession } from './auth/createSession.mutation';
 export { default as deleteSession } from './auth/deleteSession.mutation';
+
 export { default as sendMessage } from './messages/send.mutation';
 export { default as deleteMessage } from './messages/delete.mutation';
+// export { default as updateMessage } from './messages/update.mutation';
+
+export { default as createChannel } from './channels/create.mutation';
+export { default as deleteChannel } from './channels/delete.mutation';
+export { default as updateChannel } from './channels/update.mutation';
