04f81412b9686a59f444d1e9f15b2cc63fc73f92
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-23 20:40:11 +0200

    Fix uris

--- src/services/serviceURI.ts
+++ src/services/serviceURI.ts
@@ -1,6 +1,6 @@
 const serviceURIs = new Map();
 serviceURIs.set('USER_SERVICE_URI', 'http://user-service:7100/api/v1');
-serviceURIs.set('MESSAGE_SERVICE_URI', 'http://message-service:7200/api/v1');
+serviceURIs.set('MESSAGE_SERVICE_URI', 'http://message-service:7102/api/v1');
 serviceURIs.set('PROJECT_SERVICE_URI', 'http://project-service:7300/api/v1');
 
 export default serviceURIs;
